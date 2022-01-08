f9c40d07240a7dd8d2e7f9a49bf33f7686063320
ghun131 <hungdh131@gmail.com>
2022-01-08 16:27:37 +0700

    detroy article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,11 +11,21 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
       flash[:notice] = 'Article was created successfully.'
-      redirect_to @article
+      redirect_to articles_path
     else
       render 'new'
     end
